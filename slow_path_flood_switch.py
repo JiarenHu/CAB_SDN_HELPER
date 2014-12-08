63a512c057e2bedb64a60f07202e1312ba11e82f
Zishuo <zishuo.wung@gmail.com>
2014-12-08 14:13:39 -0500

    deleted:    simple_switch_13.py.bak 	modified:   slow_path_flood_switch.py

--- slow_path_flood_switch.py
+++ slow_path_flood_switch.py
@@ -1,17 +1,3 @@
-# Copyright (C) 2011 Nippon Telegraph and Telephone Corporation.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-# implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 import time
 from ryu.base import app_manager
 from ryu.controller import ofp_event
@@ -22,11 +8,11 @@ from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 
 
-class SimpleSwitch13(app_manager.RyuApp):
+class SlowPathFloodSwitch(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        super(SimpleSwitch13, self).__init__(*args, **kwargs)
+        super(SlowPathFloodSwitch, self).__init__(*args, **kwargs)
         self.mac_to_port = {}
         self.t0 = time.time()
         self.byte_cnt = 0
